d06a1b8de227e094c9696f8c23e8305a4e150aa5
Sharath Kamlekar <42378552+Dexterzprotege@users.noreply.github.com>
2022-02-02 23:47:30 +0530

    Update 02FindAllAnagramsInaString.py

--- 02FindAllAnagramsInaString.py
+++ 02FindAllAnagramsInaString.py
@@ -1,5 +1,37 @@
 # Question: https://leetcode.com/problems/find-all-anagrams-in-a-string/
 
+# ------------------------------------------------------------------------------------------------------------------ #
+
+# Solution 2: Sliding Window Technique
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        def frequency(s):
+            count = [0]*26
+            for i in range(len(s)):
+                count[ord(s[i]) - ord('a')] += 1
+            return count
+        ans = []
+        if len(p) > len(s):
+            return ans
+        n = len(s)
+        m = len(p)
+        count = frequency(p)
+        currentCount = frequency(s[0: m])
+        if count == currentCount:
+            ans.append(0)
+        for i in range(m, n):
+            currentCount[ord(s[i-m]) - ord('a')] -= 1
+            currentCount[ord(s[i]) - ord('a')] += 1
+            if count == currentCount:
+                ans.append(i-m+1)
+        return ans
+    
+# Verdict:
+# Runtime: 126 ms, faster than 77.38% of Python3 online submissions for Find All Anagrams in a String.
+# Memory Usage: 15.3 MB, less than 10.05% of Python3 online submissions for Find All Anagrams in a String.
+
+# ------------------------------------------------------------------------------------------------------------------ #    
+
 # Solution 1: Brute Force
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
@@ -14,3 +46,5 @@ class Solution:
 
 # Verdict:
 TLE
+
+# ------------------------------------------------------------------------------------------------------------------ #
