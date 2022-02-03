90b80342bf1fb123f7536c5d5b732fcc8c026b05
Sharath Kamlekar <42378552+Dexterzprotege@users.noreply.github.com>
2022-02-03 22:24:31 +0530

    Update 03FourSumII.py

--- 03FourSumII.py
+++ 03FourSumII.py
@@ -1,5 +1,46 @@
 # Question: https://leetcode.com/problems/4sum-ii/
 
+# -------------------------------------------------------------------------------------------------------------- #
+
+# Solution 3: Removing two loops O(N^2)
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        count = 0
+        hm = defaultdict(int)
+        for k in nums3:
+            for l in nums4:
+                hm[k+l] += 1
+        for i in nums1:
+            for j in nums2:
+                if hm[-(i+j)]:
+                    count += hm[-(i+j)]
+        return count
+    
+# Verdict:
+# Runtime: 1167 ms, faster than 30.85% of Python3 online submissions for 4Sum II.
+# Memory Usage: 14.1 MB, less than 99.75% of Python3 online submissions for 4Sum II.
+
+# -------------------------------------------------------------------------------------------------------------- #
+
+# Solution 2: Removing one loop O(N^3)
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        count = 0
+        hm = defaultdict(int)
+        for l in nums4:
+            hm[l] += 1
+        for i in nums1:
+            for j in nums2:
+                for k in nums3:
+                    if hm[-(i+j+k)]:
+                        count += hm[-(i+j+k)]
+        return count
+    
+# Verdict:
+TLE
+
+# -------------------------------------------------------------------------------------------------------------- #
+
 # Solution 1: Brute Force O(N^4)
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
@@ -14,3 +55,5 @@ class Solution:
 
 # Verdict:
 TLE
+
+# -------------------------------------------------------------------------------------------------------------- #
